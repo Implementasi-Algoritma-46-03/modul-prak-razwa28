1e2a7c40f37ba32f5e84882aacf27048cb3c31ef
Muhammad Razwa Rizky Syakir <razwarizky13@gmail.com>
2025-09-29 07:23:13 +0700

    update

--- Modul03/TP02.java
+++ Modul03/TP02.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TP02 {
+public class TP02b {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String nama = sc.next();
