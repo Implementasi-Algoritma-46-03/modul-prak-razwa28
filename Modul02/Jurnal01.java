dd4888a06ca94917c323ab7387e54415f6cd6eaa
Muhammad Razwa Rizky Syakir <razwarizky13@gmail.com>
2025-09-29 07:24:02 +0700

    update

--- Modul02/Jurnal01.java
+++ Modul02/Jurnal01.java
@@ -1,4 +1,4 @@
-public class Jurnal01 {
+public class Jurnal01a {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
--- Modul02/Jurnal03.java
+++ Modul02/Jurnal03.java
@@ -1,4 +1,4 @@
-public class Jurnal03 {
+public class Jurnal03a {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
