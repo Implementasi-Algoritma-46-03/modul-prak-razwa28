edc041a9d58241c1d4b3ba747544943e7b72ec9d
Muhammad Razwa Rizky Syakir <razwarizky13@gmail.com>
2025-09-29 07:28:53 +0700

    Refactor TP01, TP02, and TP03

--- Modul03/Jurnal01.java
+++ Modul03/Jurnal01.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Jurnal01a {
+public class Jurnal01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String angka = sc.nextLine();
--- Modul03/Jurnal03.java
+++ Modul03/Jurnal03.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Jurnal03a {
+public class Jurnal03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double a = sc.nextDouble();
