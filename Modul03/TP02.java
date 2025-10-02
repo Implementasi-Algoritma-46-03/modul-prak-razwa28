b9ff5b4d0cb88d44971118c59de88de372319613
Muhammad Razwa Rizky Syakir <razwarizky13@gmail.com>
2025-10-02 17:07:27 +0700

    Jurnal

--- Modul03/TP02.java
+++ Modul03/TP02.java
@@ -8,6 +8,6 @@ public class TP02 {
         int harga = sc.nextInt();
 
         int total = jumlah * harga;
-        System.out.println("Hi, " + nama + ". Total belanja adalah " + total + " rupiah");
+        System.out.println("Hi, " + nama + ". Total belanja adalah " + total + " rupiah.");
     }
 } 
