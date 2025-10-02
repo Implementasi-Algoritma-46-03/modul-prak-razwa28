fdd4ba0d45412c99f37ba74bf2fce722d1581f2d
Muhammad Razwa Rizky Syakir <razwarizky13@gmail.com>
2025-10-02 16:59:34 +0700

    update jurnal 01

--- Modul03/Jurnal01.java
+++ Modul03/Jurnal01.java
@@ -4,8 +4,11 @@ public class Jurnal01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String angka = sc.nextLine();
-        System.out.println(angka.charAt(0));
-        System.out.println(angka.substring(1));
-        sc.close();
-    }
+
+        String depan = angka.substring(0, angka.length() - 3);
+        String belakang = angka.substring(angka.length() - 3);
+
+        System.out.println(depan);
+        System.out.println(belakang);
+    } 
 }
