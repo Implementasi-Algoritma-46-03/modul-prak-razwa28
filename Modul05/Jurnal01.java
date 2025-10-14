6ee8776546ba5db276c1eb6ac9d0433432f50d43
Muhammad Razwa Rizky Syakir <razwarizky13@gmail.com>
2025-10-14 22:34:56 +0700

    update

--- Modul05/Jurnal01.java
+++ Modul05/Jurnal01.java
@@ -1,16 +1,35 @@
-import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class Jurnal01 {
     public static void main(String[] args) {
-        // Ganti sesuai test case
-        String namaMakanan = "Nasi Goreng";
-        int jumlah = 3;
-        int harga = 15000;
+        Scanner Scanner = new Scanner(System.in);
+        int kode = Scanner.nextInt();
+        int jumlah = Scanner.nextInt();
 
-        int total = jumlah * harga;
-        DecimalFormat df = new DecimalFormat("#,###");
-        String totalFormat = df.format(total).replace(",", ".");
-
-        System.out.println(namaMakanan + " " + jumlah + " buah, total harga Rp. " + totalFormat);
+        String namaMenu = "";
+        int harga = 0;
+        switch (kode) {
+            case 1:
+                namaMenu = "Nasi Goreng";
+                harga = 15;
+                break;
+            case 2:
+                namaMenu = "Mie Goreng / Nyemek";
+                harga = 18;
+                break;
+            case 3:
+                namaMenu = "Kwetiau Goreng / Nyemek";
+                harga = 20;
+                break;
+            case 4:
+                namaMenu = "Capcay Goreng / Kuah";
+                harga = 23;
+                break;
+            default:
+                System.out.print("Kode menu tidak valid!");
+                        return;
+        }
+        int totalHarga = harga * jumlah;
+        System.out.print(namaMenu + " " + jumlah + " buah, total harga Rp. " + totalHarga + ".000");
     }
 }
--- Modul05/Jurnal02.java
+++ Modul05/Jurnal02.java
@@ -1,19 +1,63 @@
 import java.util.Scanner;
 
 public class Jurnal02 {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner input = new Scanner(System.in);
+        
         String hari = input.nextLine();
-
+        int bilanganBulat = input.nextInt();
+        
+        int HariSekarang = 0;
         switch (hari) {
-            case "Senin": System.out.println("Selasa"); break;
-            case "Selasa": System.out.println("Rabu"); break;
-            case "Rabu": System.out.println("Kamis"); break;
-            case "Kamis": System.out.println("Jum'at"); break;
-            case "Jum'at": System.out.println("Sabtu"); break;
-            case "Sabtu": System.out.println("Minggu"); break;
-            case "Minggu": System.out.println("Senin"); break;
-            default: System.out.println("Hari tidak valid");
+            case "Senin":
+                HariSekarang = 1;
+                break;
+            case "Selasa":
+                HariSekarang = 2;
+                break;
+            case "Rabu":
+                HariSekarang = 3;
+                break;
+            case "Kamis":
+                HariSekarang = 4;
+                break;
+            case "Jum'at":
+                HariSekarang = 5;
+                break;
+            case "Sabtu":
+                HariSekarang = 6;
+                break;
+            case "Minggu":
+                HariSekarang = 7;
+                break;
         }
-    }
+
+
+        int HariSetelahbilanganBulat = (bilanganBulat%7) + HariSekarang;
+
+
+        switch (HariSetelahbilanganBulat) {
+            case 1:
+                System.out.println("Senin");
+                break;
+            case 2:
+                System.out.println("Selasa");
+                break;
+            case 3:
+                System.out.println("Rabu");
+                break;
+            case 4:
+                System.out.println("Kamis");
+                break;
+            case 5:
+                System.out.println("Jum'at");
+                break;
+            case 6:
+                System.out.println("Sabtu");
+                break;
+            case 7:
+                System.out.println("Minggu");
+                break;
+ }
+}
 }
