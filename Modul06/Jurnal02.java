18cb8d0bfff4f238b87343199ad277b585cd9509
Muhammad Razwa Rizky Syakir <razwarizky13@gmail.com>
2025-10-23 15:59:08 +0700

    jurnal

--- Modul06/Jurnal02.java
+++ Modul06/Jurnal02.java
@@ -1,6 +1,36 @@
-public class Jurnal02 {
+import java.util.Scanner;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class Jurnal02 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String lirik = in.nextLine();
+        
+        // Hilangkan spasi agar sesuai contoh soal
+        String tanpaSpasi = lirik.replace(" ", "");
+        int panjang = tanpaSpasi.length();
+        int sisa = panjang % 5;
+        
+        String vokal = "";
+        if (sisa == 0) {
+            vokal = "a";
+        } else if (sisa == 1) {
+            vokal = "e";
+        } else if (sisa == 2) {
+            vokal = "i";
+        } else if (sisa == 3) {
+            vokal = "o";
+        } else if (sisa == 4) {
+            vokal = "u";
+        }
+
+        // Ganti semua huruf vokal (baik kecil maupun besar)
+        String hasil = lirik
+            .replaceAll("[aA]", vokal)
+            .replaceAll("[iI]", vokal)
+            .replaceAll("[uU]", vokal)
+            .replaceAll("[eE]", vokal)
+            .replaceAll("[oO]", vokal);
+
+        System.out.println(hasil);
     }
 }
--- Modul06/Jurnal03.java
+++ Modul06/Jurnal03.java
@@ -1,6 +1,17 @@
-public class Jurnal03 {
+import java.util.Scanner;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class Jurnal03 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        double r = in.nextDouble(); // jari-jari
+        double h = in.nextDouble(); // tinggi tabung
+        double t = in.nextDouble(); // tinggi air
+
+        double vMax = Math.PI * r * r * h;
+        double vAir = Math.PI * r * r * t;
+        double persen = (t / h) * 100;
+
+        System.out.printf("%.2f %.2f %.1f%%", vMax, vAir, persen);
     }
 }
