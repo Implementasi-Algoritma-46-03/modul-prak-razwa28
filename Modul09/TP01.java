a563ac44da738c9c45ec08776f0ece416926fa30
Muhammad Razwa Rizky Syakir <razwarizky13@gmail.com>
2025-11-08 19:02:44 +0700

    update modul09

--- Modul09/TP01.java
+++ Modul09/TP01.java
@@ -1,6 +1,17 @@
+import java.util.*;
+
 public class TP01 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+
+        for(int i = 1; i <= a; i++){
+            if (i %2 == 0) {
+                System.out.println(i +" Genap");
+            } else {
+                System.out.println(i +" Ganjil");
+            }
+        }
     }
 }
--- Modul09/TP03.java
+++ Modul09/TP03.java
@@ -1,6 +1,17 @@
+import java.util.*;
+
 public class TP03 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.close();
+
+        long hasil = 1;
+
+        for(int i = 1; i <= n; i++){
+            hasil = hasil * i;
+        }
+        System.out.println(hasil);
     }
 }
