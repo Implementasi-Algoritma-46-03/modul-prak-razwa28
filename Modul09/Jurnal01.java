b42a80658697e070ab72cd8d8c0f646d80c506bc
Muhammad Razwa Rizky Syakir <razwarizky13@gmail.com>
2025-11-13 16:25:31 +0700

    o

--- Modul09/Jurnal01.java
+++ Modul09/Jurnal01.java
@@ -2,7 +2,7 @@ public class Jurnal01 {
 
     public static void main(final String[] args) {
         for(int i = 1; i <= 10; i++){
-            System.out.println(i*i);
+            System.out.print(i*i);
         }
     }
 }
