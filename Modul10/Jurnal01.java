8c9740b0cde34d75d06a6d852092d3eca71e8387
Muhammad Razwa Rizky Syakir <razwarizky13@gmail.com>
2025-11-20 16:51:21 +0700

    update

--- Modul10/Jurnal01.java
+++ Modul10/Jurnal01.java
@@ -1,19 +1,17 @@
 public class Jurnal01 {
     public static void main(String[] args) {
-        int angka = 1;
 
         for (int i = 1; i <= 6; i++) {
 
-            if (i % 2 == 0) {
-                System.out.print(" ");
-            }
-
             for (int j = 1; j <= 5; j++) {
-                System.out.print(angka + " ");
-            }
 
+                if (i % 2 == 0) {
+                    System.out.print(" " + i);
+                }else {
+                    System.out.print(i + " ");
+                }
+            }
             System.out.println();
-            angka++;
         }
     }
 }
