c6cb2c9ed70a781536d8b67857777b31e78ccd93
Muhammad Razwa Rizky Syakir <razwarizky13@gmail.com>
2025-12-04 15:54:30 +0700

    update

--- Modul12/Jurnal01.java
+++ Modul12/Jurnal01.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
 public class Jurnal01 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        Scanner s = new Scanner(System.in);
+
+        int banyakArr = s.nextInt();
+        int[][] arr1 = input(s, banyakArr);
+
+        
+        for (int i = arr1.length-1; i >= 0; i--) {
+            for (int j = 0; j < arr1.length; j++) {
+                System.out.print(arr1[i][j]);
+                if (j < arr1.length - 1) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+
+    private static int[][] input(Scanner s, int n) {
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                arr[i][j] = s.nextInt();
+            }
+        }
+        return arr;
     }
 }
--- Modul12/Jurnal03.java
+++ Modul12/Jurnal03.java
@@ -1,6 +1,45 @@
+import java.util.Scanner;
 public class Jurnal03 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        Scanner s = new Scanner(System.in);
+
+        int banyakArr = s.nextInt();
+        int[][] arr = new int[banyakArr][banyakArr];
+
+        int atas = 0;
+        int bawah = banyakArr-1;
+        int kiri = 0;
+        int kanan = banyakArr-1;
+
+        int counter = 1;
+        while (counter <= banyakArr*banyakArr) {
+            for (int i = atas; i <= bawah; i++) {       // Atas - bawah
+                arr[i][kiri] = counter++;
+            }
+            kiri++;
+            for (int i = kiri; i <= kanan; i++) {        // Kiri - kanan
+                arr[bawah][i] = counter++;
+            }
+            bawah--;
+            for (int i = bawah; i >= atas; i--) {              // Bawah - atas
+                arr[i][kanan] = counter++;
+            }
+            kanan--;
+            for (int i = kanan; i >= kiri; i--) {                 // Kanan - kiri
+                arr[atas][i] = counter++;
+            }
+            atas++;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (j>=1)
+                    System.out.print(" ");
+                System.out.print(arr[i][j]);
+            }
+            System.out.println("");
+        }
     }
 }
