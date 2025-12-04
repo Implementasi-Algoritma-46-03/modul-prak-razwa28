1f9f801f71a1e02f6483e4560c42b4549029a7e3
Muhammad Razwa Rizky Syakir <razwarizky13@gmail.com>
2025-12-04 15:59:36 +0700

    update lagi

--- Modul12/Jurnal02.java
+++ Modul12/Jurnal02.java
@@ -1,90 +1,85 @@
 import java.util.Scanner;
 
 public class Jurnal02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-    public static void main(final String[] args) {
-        Scanner s = new Scanner(System.in);
-
-        int N = s.nextInt();
-        int[][] matriks = new int[N][N];
-
-        // Input matriks
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                matriks[i][j] = s.nextInt();
-            }
-        }
-
-        int hasil = pemenang(matriks);
-
-        if (hasil == 1) {
-            System.out.println("O");
-        } 
-        else if (hasil == 2) {
-            System.out.println("X");
-        } 
-        else {
-            if (seri(matriks)) {
-                System.out.println("-");
-            } else {
-                System.out.println("?");
-            }
-        }
-    }
-
-    // Menentukan pemenang tic-tac-toe
-    private static int pemenang(int[][] matriks) {
-
-        // Cek baris
-        for (int i = 0; i < matriks.length; i++) {
-            if (matriks[i][0] == matriks[i][1] &&
-                matriks[i][1] == matriks[i][2] &&
-                matriks[i][0] != 0) 
-            {
-                return matriks[i][0];
-            }
-        }
-
-        // Cek kolom
-        for (int j = 0; j < matriks.length; j++) {
-            if (matriks[0][j] == matriks[1][j] &&
-                matriks[1][j] == matriks[2][j] &&
-                matriks[0][j] != 0) 
-            {
-                return matriks[0][j];
-            }
-        }
-
-        // Cek diagonal utama
-        if (matriks[0][0] == matriks[1][1] &&
-            matriks[1][1] == matriks[2][2] &&
-            matriks[0][0] != 0) 
-        {
-            return matriks[0][0];
-        }
-
-        // Cek diagonal kedua
-        if (matriks[2][0] == matriks[1][1] &&
-            matriks[1][1] == matriks[0][2] &&
-            matriks[2][0] != 0) 
-        {
-            return matriks[2][0];
-        }
-
-        return 0; // Tidak ada pemenang
-    }
-
-    // Mengecek apakah seri
-    private static boolean seri(int[][] matriks) {
-
-        for (int i = 0; i < matriks.length; i++) {
-            for (int j = 0; j < matriks.length; j++) {
-                if (matriks[i][j] == 0) {
-                    return false; // Masih ada ruang kosong → belum seri
+        int ukuranPapan = input.nextInt();
+        int[][] papan = new int[ukuranPapan][ukuranPapan];
+        
+        boolean adaTempatKosong = false;
+        for (int i = 0; i < ukuranPapan; i++) {
+            for (int j = 0; j < ukuranPapan; j++) {
+                papan[i][j] = input.nextInt();
+                if (papan[i][j] == 0) {
+                    adaTempatKosong = true;
                 }
             }
         }
 
-        return true; // Tidak ada 0 → papan penuh → seri
+        int pemenang = 0; 
+
+        for (int i = 0; i < ukuranPapan; i++) {
+            
+            boolean barisSama = true;
+            int nilaiBaris = papan[i][0];
+            if (nilaiBaris != 0) {
+                for (int j = 1; j < ukuranPapan; j++) {
+                    if (papan[i][j] != nilaiBaris) {
+                        barisSama = false;
+                        break;
+                    }
+                }
+                if (barisSama) pemenang = nilaiBaris;
+            }
+
+            boolean kolomSama = true;
+            int nilaiKolom = papan[0][i];
+            if (nilaiKolom != 0) {
+                for (int j = 1; j < ukuranPapan; j++) {
+                    if (papan[j][i] != nilaiKolom) {
+                        kolomSama = false;
+                        break;
+                    }
+                }
+                if (kolomSama) pemenang = nilaiKolom;
+            }
+        }
+
+        boolean diagUtamaSama = true;
+        int nilaiDiagUtama = papan[0][0];
+        if (nilaiDiagUtama != 0) {
+            for (int i = 1; i < ukuranPapan; i++) {
+                if (papan[i][i] != nilaiDiagUtama) {
+                    diagUtamaSama = false;
+                    break;
+                }
+            }
+            if (diagUtamaSama) pemenang = nilaiDiagUtama;
+        }
+
+        boolean diagSampingSama = true;
+        int nilaiDiagSamping = papan[0][ukuranPapan - 1];
+        if (nilaiDiagSamping != 0) {
+            for (int i = 1; i < ukuranPapan; i++) {
+                if (papan[i][ukuranPapan - 1 - i] != nilaiDiagSamping) {
+                    diagSampingSama = false;
+                    break;
+                }
+            }
+            if (diagSampingSama) pemenang = nilaiDiagSamping;
+        }
+
+        if (pemenang == 1) {
+            System.out.println("O");
+        } else if (pemenang == 2) {
+            System.out.println("X");
+        } else if (adaTempatKosong) {
+            System.out.println("?");
+        } else {
+            System.out.println("-"); 
+        }
+        
+        input.close();
     }
 }
