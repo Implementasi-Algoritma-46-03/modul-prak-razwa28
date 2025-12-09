74f5113cf821339b9d56bcb2266a9fab82d84efe
Muhammad Razwa Rizky Syakir <razwarizky13@gmail.com>
2025-12-09 23:25:39 +0700

    update

--- Modul13/TP01.java
+++ Modul13/TP01.java
@@ -1,6 +1,52 @@
+import java.util.Scanner;
+
 public class TP01 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        Scanner s = new Scanner(System.in);
+
+        int n = 10;
+        
+        double[] gel1 = new double[n];
+        double[] gel2 = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            gel1[i] = s.nextDouble();
+        }
+
+        for (int i = 0; i < n; i++) {
+            gel2[i] = s.nextDouble();
+        }
+
+        for (int i = 1; i < n; i++) {
+            double kunci = gel1[i];
+            int j = i;
+            for (; j > 0 && gel1[j - 1] > kunci; j--) {
+                gel1[j] = gel1[j - 1];
+            }
+            gel1[j] = kunci;
+        }
+
+        for (int i = 1; i < n; i++) {
+            double kunci = gel2[i];
+            int j = i;
+            for (; j > 0 && gel2[j - 1] > kunci; j--) {
+                gel2[j] = gel2[j - 1];
+            }
+            gel2[j] = kunci;
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("%.2f", gel1[i]);
+            if (i < n - 1) System.out.print(" ");
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("%.2f", gel2[i]);
+            if (i < n - 1) System.out.print(" ");
+        }
     }
 }
--- Modul13/TP03.java
+++ Modul13/TP03.java
@@ -1,6 +1,33 @@
+import java.util.Scanner;
+
 public class TP03 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+
+        int[] data = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            data[i] = s.nextInt();
+        }
+
+        int kunci = s.nextInt();
+        int sementara = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (data[i] == kunci) {
+                sementara = i;
+                break;
+            }
+        }
+
+        if (sementara != -1) {
+            System.out.println("Ditemukan di indeks ke-" + sementara);
+        } else {
+            System.out.println("Tidak ditemukan");
+        }
     }
 }
