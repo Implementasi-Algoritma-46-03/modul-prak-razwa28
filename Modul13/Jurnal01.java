cd80d150f9724703601fe05a78123f22d3cb4e7c
Muhammad Razwa Rizky Syakir <razwarizky13@gmail.com>
2025-12-11 15:59:08 +0700

    update

--- Modul13/Jurnal01.java
+++ Modul13/Jurnal01.java
@@ -1,6 +1,33 @@
+import java.util.Scanner;
+
 public class Jurnal01 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        String[] b = new String[a];
+
+        for (int i = 0; i < b.length; i++) { // membaca setiap kata dan memasukkannya ke dalam array
+            b[i]= scan.next().trim(); // menghapus spasi berlebihan didepan satu atau belakang
+        }
+
+        for (int i = 0; i < b.length; i++) {
+            String key = b[i]; // elemen yang sedang diurutkan 
+            int j = i - 1; 
+
+            while(j >= 0 && b[j].compareToIgnoreCase(key) < 0){ // membandingkan string tanpa melihat besar kecil hurufnya
+                b[j + 1] = b[j]; // jika keil maka
+                j -= 1;
+            }
+
+            b[j + 1] = key;
+        }
+
+        for (int i = 0; i < b.length; i++) {
+            System.out.printf("%s ", b[i]);
+        }
+
+        scan.close();
     }
 }
--- Modul13/Jurnal03.java
+++ Modul13/Jurnal03.java
@@ -1,6 +1,31 @@
-public class Jurnal03 {
+import java.util.Scanner;
 
+public class Jurnal03 {
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        String[] b = new String[a]; // Array 2D
+
+        for (int i = 0; i < b.length; i++) {
+            b[i]= scan.next().trim();
+        }
+
+        String c = scan.next().trim();
+        boolean isFound = false;
+
+        for (int i = 0; i < b.length; i++) {
+            if(b[i].equals(c)){
+                System.out.printf("Ditemukan di indeks ke-%d", i + 1);
+                isFound = true;
+                break;
+            }     
+        }
+        if(!isFound){
+            System.out.print("Data tidak ditemukan");
+        }
+
+        scan.close();
     }
 }
